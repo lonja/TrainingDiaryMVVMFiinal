1b308abcac662b2abfdc95b19cab5dcbcaff055d
lonja <lonja007@gmail.com>
2016-06-06 13:53:05 +0300

    navigation drawer changed

--- app/src/main/java/su/dreamteam/lonja/trainingdiaryfinal/ui/activity/MeasurementsActivity.java
+++ app/src/main/java/su/dreamteam/lonja/trainingdiaryfinal/ui/activity/MeasurementsActivity.java
@@ -111,17 +111,17 @@ public class MeasurementsActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_exercises) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_trainings) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_measurements) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_statistics) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
