52db752218346992983466059aa21409e3c71415
lonja <lonja007@gmail.com>
2016-06-11 19:41:36 +0300

    repos created

---
+++ data/src/main/java/su/dreamteam/lonja/data/repository/ExercisesRepository.java
@@ -0,0 +1,51 @@
+package su.dreamteam.lonja.data.repository;
+
+import android.support.annotation.NonNull;
+
+import io.realm.RealmResults;
+import rx.Observable;
+import su.dreamteam.lonja.data.model.Exercise;
+import su.dreamteam.lonja.data.source.ExercisesDataSource;
+
+public class ExercisesRepository implements ExercisesDataSource {
+
+    private ExercisesDataSource mLocalDataSource;
+
+    private static ExercisesRepository INSTANCE;
+
+    private ExercisesRepository(ExercisesDataSource localDataSource) {
+        mLocalDataSource = localDataSource;
+    }
+
+    public static ExercisesRepository getInstance(ExercisesDataSource localDataSource) {
+        if (INSTANCE == null) {
+            INSTANCE = new ExercisesRepository(localDataSource);
+        }
+        return INSTANCE;
+    }
+
+    @Override
+    public Observable<RealmResults<Exercise>> getExercises() {
+        return mLocalDataSource.getExercises();
+    }
+
+    @Override
+    public Observable<Exercise> getExercise(String exerciseId) {
+        return mLocalDataSource.getExercise(exerciseId);
+    }
+
+    @Override
+    public Observable<Exercise> getExercise(int exercise) {
+        return mLocalDataSource.getExercise(exercise);
+    }
+
+    @Override
+    public Observable saveExercise(@NonNull Exercise exercise) {
+        return mLocalDataSource.saveExercise(exercise);
+    }
+
+    @Override
+    public Observable deleteExercise(@NonNull String exerciseId) {
+        return mLocalDataSource.deleteExercise(exerciseId);
+    }
+}
