20179682224281774e564d1656743b610827184d
lonja <lonja007@gmail.com>
2016-06-13 16:15:28 +0300

    decoration class added

---
+++ app/src/main/java/su/dreamteam/lonja/trainingdiaryfinal/ui/decorator/ListItemDecorator.java
@@ -0,0 +1,46 @@
+package su.dreamteam.lonja.trainingdiaryfinal.ui.decorator;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class ListItemDecorator extends RecyclerView.ItemDecoration {
+
+    private Drawable mDrawable;
+
+    public ListItemDecorator(Drawable drawable) {
+        mDrawable = drawable;
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int dividerLeft = parent.getPaddingLeft();
+        int dividerRight = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount - 1; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int dividerTop = child.getBottom() + params.bottomMargin;
+            int dividerBottom = dividerTop + mDrawable.getIntrinsicHeight();
+
+            mDrawable.setBounds(dividerLeft, dividerTop, dividerRight, dividerBottom);
+            mDrawable.draw(c);
+        }
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+
+        if (parent.getChildAdapterPosition(view) == 0) {
+            return;
+        }
+
+        outRect.top = mDrawable.getIntrinsicHeight();
+    }
+}
